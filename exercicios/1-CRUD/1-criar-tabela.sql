9031911096da77173d2c52a109c8e757f05946db
celsorma102 <celsorma102@gmail.com>
2025-09-02 10:05:31 -0300

    Etapa CRUD Concluida!

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,1 +1,8 @@
 -- Crie uma nova tabela no banco de dados
+CREATE TABLE salario_classe(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nivel VARCHAR(50),
+  salario FLOAT
+);
+-- DROP TABLE salario_classe;
+-- DROP TABLE salario_classe_final;
--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,11 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
+INSERT INTO salario_classe (id, nivel, salario)
+VALUES
+(1, 'Estagiario', 2000),
+(2, 'Junior', 6000),
+(3, 'Pleno', 10000);
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO salario_classe (nivel, salario)
+VALUES
+ ('Senior', 12000);
